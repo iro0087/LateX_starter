3e7596628e0b1232e6368ebbc5dab3d3dc0056ab
iro0087 <114911243+iro0087@users.noreply.github.com>
2023-06-21 00:52:35 +0200

    Add files via upload

---
+++ dwm_bar.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+while :
+do
+        xsetroot -name "$(echo CPU_TEMP: $(numfmt --from-unit=K --to-unit=M --format "%8.0f" $(cat /sys/class/thermal/thermal_zone0/temp))°C) | $(echo RAM: $(echo $(numfmt --from-unit=K --to-unit=G --format "%8.1f" $(free | awk '/^Mem:/ {print $3}'))Gi)"/"$(echo $(numfmt --to-unit=G --format "%8.1f" $(free | awk '/^Mem:/ {print $2}'))Gi)) | $(date) | 📢 $(awk -F"[][]" '/Left:/ { print $2}' <(amixer sget Master))"
+        sleep 0.1
+done
+
